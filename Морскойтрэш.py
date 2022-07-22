4fdd4d1ca49387d24e0b76312ae6baa51f4bb05a
miumiu <pavlovNP1992@yandex.ru>
2022-07-22 07:27:29 +0300

    12

--- Морскойтрэш.py
+++ Морскойтрэш.py
@@ -1,65 +1,66 @@
-#координаты
+#4 класса исключения
+class BoardException(Exception):
+    pass
+
+class BoardOutException(BoardException):
+    def __str__(self):
+        return "Вы пытаетесь выстрелить за доску!"
+
+class BoardUsedException(BoardException):
+    def __str__(self):
+        return "Вы уже стреляли в эту клетку"
+
+class BoardWrongShipException(BoardException):
+    pass
+#класс точки
 class Dot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
     def __eq__(self, other):
-        return self.x == other.x and self.y ==other.y
-
+        return self.x == other and self.y == other
     def __repr__(self):
-        return f'Dot({self.x}, {self.y})'
-
-#исключения
-
-class BoardException(Exception):
-    def __str__(self):
-        return "Вы стреляете по берегу!"
-    def __str__(self):
-        return "Сюда уже стреляли"
-
-class BoardWrongShipException(Exception):
-    pass
-
-#кораблик и попадания
+        return f'({self.x},{self.y})'
+#корабль
 class Ship:
-    def __init__(self, bow, i, o):
-        self.bow = bow
-        self.i = i
-        self.o = o
-        self.lives =1
+    def __init__(self,ship_length, bow_ship, ship_direction):
+        self.ship_lenght = ship_length
+        self.bow_ship = bow_ship
+        self.ship_direction = ship_direction
+        self.hit_point = ship_length
+    #декоратор для определения направления корабля, храниния списка всех точек и точки попадания
     @property
     def dots(self):
         ship_dots = []
-        for i  in range(self.i):
-            cur_x = self.bow.x
-            cur_y = self.bow.y
-            if self.o == 0:
-                cur_x += 1
-            elif self.o == 1
-                cur_y += 1
-        return  ship_dots
+            for i in range(self.ship_length)
+                point_x = self.bow_ship.x
+                point_y = self.bow_ship.y
+                if self.ship_direction == 0:
+                    point_x += i
+                elif self.ship_direction == 1:
+                    point_y += i
+            ship_dots.append(Dot(point_x,point_y))
+        return ship_dots
 
     def shooten(self, shot):
         return shot in self.dots
 
-# вывод доски через класс
+#доска
 class Board:
-    def __init__(self, hid = False, size = 6 ):
-        self.size = size
+    def __init__(self, hid=False, size_board = 6):
+        #скрывает доску
         self.hid = hid
-        #количество кораблей
+        #размер доски
+        self.size_board = size_board
         self.count = 0
+        #список строк поля
+        self.field = [['O'] * size_board for _ in range(size_board)]
+        #список занятых точек
+        self.busy_point = []
+        #список точек с кораблями
+        self.ships_point = []
+    def app_ship(self, ship):
+        for d in ship.dots:
+            if self.out(d) or d in self.busy_point:
+                return BoardWrongShipException()
 
-        self.field = [["O"] * size for _ in range(size)]
-
-        self.buzy= []
-        self.ships =[]
-    def __str__(self):
-        res =""
-        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
-        for i, row in enumerate(self.field):
-            res += f"\n{i + 1 } | " + " | ".join(row) + " |"
-        if self.hid:
-            res = res.replace("■", "O")
-        return  res
